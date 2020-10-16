da44b01cd70c4d897fd99bf8becfe0c1251cfcf7
Andrey Khomutov <homutovan@live.com>
2020-10-17 05:50:10 +0700

    update class_form.js

---
+++ src/js/class_form.js
@@ -0,0 +1,17 @@
+/* eslint class-methods-use-this: ["error", { "exceptMethods": ["init", "close", "sent"] }] */
+export default class Form {
+  constructor(element) {
+    this.element = element;
+    this.init();
+  }
+
+  init() {}
+
+  close() {
+
+
+    
+  }
+
+  sent() {}
+}
